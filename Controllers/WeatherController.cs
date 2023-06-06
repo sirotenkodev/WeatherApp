4bfcb366514a29a01df65974d68a9b2a2d1271c3
Nikita Sirotenko <nick.sirotenko@yandex.ru>
2023-06-06 22:03:40 +0600

    Initial commit

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class WeatherController : Controller
+    {
+
+        public WeatherService weatherService { get; set; }
+
+        public WeatherController()
+        {
+            weatherService = new WeatherService();
+        }
+        public IActionResult Weather()
+        {
+            string uri = "http://api.openweathermap.org/data/2.5/weather?q=Omsk&units=metric&appid=01351d08df9240a33b4bf6cbd923b7dd";
+            WeatherData results = weatherService.GetWeatherData(uri).Result;
+            return View(results);
+        }
+    }
+}
---
+++ Services/WeatherService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+
+namespace WeatherApp
+{
+    public class WeatherService
+    {
+        HttpClient m_client;
+
+        public WeatherService()
+        {
+            m_client = new HttpClient();
+        }
+
+        public async Task<WeatherData> GetWeatherData(string query)
+        {
+            WeatherData weatherData = null;
+            try
+            {
+                var response = await m_client.GetAsync(query);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    weatherData = JsonConvert.DeserializeObject<WeatherData>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+            return weatherData;
+        }
+
+    }
+}
