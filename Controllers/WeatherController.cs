144f993ca7b0eba86ed72a5e764edf3b8c6ceb79
Nikita Sirotenko <nick.sirotenko@yandex.ru>
2023-06-07 19:55:14 +0600

    Add Post weather

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace WeatherApp
 {
+    [Route("/")]
     public class WeatherController : Controller
     {
 
@@ -16,9 +17,22 @@ namespace WeatherApp
         {
             weatherService = new WeatherService();
         }
+
+        [Route("/")]
+        [Route("/index")]
+        [Route("/weather")]
+        [HttpGet]
         public IActionResult Weather()
         {
             string uri = "http://api.openweathermap.org/data/2.5/weather?q=Omsk&units=metric&appid=APIKEY";
+            WeatherData results = weatherService.GetWeatherData(uri).Result;
+            return View(results);
+        }
+
+        [HttpPost]
+        public IActionResult Weather(string city)
+        {
+            string uri = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=APIKEY";
             WeatherData results = weatherService.GetWeatherData(uri).Result;
             return View(results);
         }
